b08bd4585e8f163a210d76d8c2e72306c08278d9
John Tuley <jtuley@salesforce.com>
2015-07-15 16:26:14 -0600

    Change symbols around.
    Uncached and cached symbols are now the same, but red and green (respectively).

--- prompt.sh
+++ prompt.sh
@@ -7,13 +7,13 @@ if [ -n "${BASH_VERSION}" ]; then
     : ${omg_ungit_prompt:=$PS1}
     : ${omg_second_line:=$PS1}
 
-    : ${omg_is_a_git_repo_symbol:=''}
-    : ${omg_has_untracked_files_symbol:=''}        #                ?    
+    : ${omg_is_a_git_repo_symbol:=''}              # 
+    : ${omg_has_untracked_files_symbol:=''}        #                ?    
     : ${omg_has_adds_symbol:=''}
     : ${omg_has_deletions_symbol:=''}
-    : ${omg_has_cached_deletions_symbol:=''}
+    : ${omg_has_cached_deletions_symbol:=''}       # 
     : ${omg_has_modifications_symbol:=''}
-    : ${omg_has_cached_modifications_symbol:=''}
+    : ${omg_has_cached_modifications_symbol:=''}   # 
     : ${omg_ready_to_commit_symbol:=''}            #   →
     : ${omg_is_on_a_tag_symbol:=''}                #   
     : ${omg_needs_to_merge_symbol:='ᄉ'}
@@ -24,7 +24,8 @@ if [ -n "${BASH_VERSION}" ]; then
     : ${omg_rebase_tracking_branch_symbol:=''}     #   
     : ${omg_merge_tracking_branch_symbol:=''}      #  
     : ${omg_should_push_symbol:=''}                #    
-    : ${omg_has_stashes_symbol:=''}
+    : ${omg_has_stashes_symbol:=''}                # 
+
 
     : ${omg_default_color_on:='\[\033[1;37m\]'}
     : ${omg_default_color_off:='\[\033[0m\]'}
